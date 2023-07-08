5ff78e5b49c2adc1770cbfdaee16e562134fc382
pavel <paveq2003@yandex.ru>
2023-07-09 02:11:37 +0300

    layout local

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,4 +1,4 @@
-import { Registration } from "./registration/Registration"
+//import { Registration } from "./registration/Registration"
 import { Authorization } from "./authorization/Authorization"
 import { Lk } from './lk/Lk'
 import "../pages/App.scss"
