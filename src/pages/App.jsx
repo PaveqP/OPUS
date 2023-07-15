4d80f5270ab968dd2f376a3ecab5a5c19171de4d
Vasilii Semenov <ivaso2004@mail.ru>
2023-07-15 23:01:31 +0300

    finish module addWork and page Settings

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,12 +1,13 @@
 import { Registration } from "./registration/Registration"
 import { Authorization } from "./authorization/Authorization"
 import { Lk } from './lk/Lk'
+import { Settings } from "./settings/Settings"
 import "../pages/App.scss"
 
 function App() {
   return (
     <div className="App">
-      <Lk />
+      <Settings />
     </div>
   );
 }
