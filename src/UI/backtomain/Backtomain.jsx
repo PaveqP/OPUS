0a7231de6a7d5f93d4af4638fddb45343fd6d820
Pavel <noonno20445@yandex.ru>
2023-07-28 00:43:00 +0300

    old layout

--- src/UI/backtomain/Backtomain.jsx
+++ src/UI/backtomain/Backtomain.jsx
@@ -4,17 +4,12 @@ function Backtomain() {
     return (
         <div className="backtomain">
             <a href="">
-<<<<<<< HEAD
-                <img src={require("../../UI/utils/img/arrowleft.png")} alt="#" className="bactomain__img" />
-                <span className="backtomain__text">главная</span>
-=======
                 <div className="backtomain__row">
                     <div className="backtomain__img">
                         <img src={require("../../UI/utils/img/arrowleft.png")} alt="#" />
                     </div>
                     <div className="backtomain__text">Главная</div>
                 </div>
->>>>>>> ba8c292d1dd221359de683509264b705f1260e73
             </a>
         </div>
     )
