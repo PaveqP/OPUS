9f17833396d2dc7b552302bbf2327c78e8455411
Pavel <noonno20445@yandex.ru>
2023-09-08 23:42:17 +0300

    projects list

--- src/pages/projects/Projects.jsx
+++ src/pages/projects/Projects.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { LkSwitcher } from '../../components/lkSwitcher/LkSwitcher'
 import { MainHeader } from '../../moduls/mainHeader/MainHeader'
+import { ProjectFilters } from '../../components/projectFilters/ProjectFilters'
+import { ProjectSmallCard } from '../../components/projectSmallCard/ProjectSmallCard'
 
 import './Projects.scss'
 
 function Projects({profile, setProfile, projects, setProjects}) {
+
+  const getFilterState = () => {
+    const filterValue = "default";
+    return filterValue;
+  };
+
+  const handleFilterChange = (e) => {
+    setFilterType(e.target.value);
+  };
+
+  const [filterType, setFilterType] = useState(getFilterState)
+
+  const [owner, setOwner] = useState(true)
+  const [member, setMember] = useState(false)
+
+  console.log(filterType)
+
   return (
     <>
         <div>
@@ -12,6 +31,36 @@ function Projects({profile, setProfile, projects, setProjects}) {
         </div>
         <div>
             <LkSwitcher profile={profile} setProfile={setProfile} projects={projects} setProjects={setProjects}/>
+        </div>
+        <div>
+            <ProjectFilters filterType={filterType} setFilterType={handleFilterChange} owner={owner} setOwner={setOwner} member={member} setMember={setMember}/>
+        </div>
+        <div>
+            <ProjectSmallCard 
+              img={require('../../UI/utils/img/project_placeholder.png')} 
+              title={'Самое крутое приложение в мире, вы вообще не представляете насколько. Опус - сказка, мечта, конфетка'} 
+              target={'Расширить деловые связи, сделать мир лучше и проще для аудитории в возрасте 16-23 лет. Мы - будущее, хотя нет, мы - настоящее!'} 
+              owner={'livermoney'} 
+              tag={'Аналитика Бизнес'}
+            />
+        </div>
+        <div>
+            <ProjectSmallCard 
+              img={require('../../UI/utils/img/project_placeholder.png')} 
+              title={'Самое крутое приложение в мире, вы вообще не представляете насколько. Опус - сказка, мечта, конфетка'} 
+              target={'Расширить деловые связи, сделать мир лучше и проще для аудитории в возрасте 16-23 лет. Мы - будущее, хотя нет, мы - настоящее!'} 
+              owner={'livermoney'} 
+              tag={'Аналитика Бизнес'}
+            />
+        </div>
+        <div>
+            <ProjectSmallCard 
+              img={require('../../UI/utils/img/project_placeholder.png')} 
+              title={'Самое крутое приложение в мире, вы вообще не представляете насколько. Опус - сказка, мечта, конфетка'} 
+              target={'Расширить деловые связи, сделать мир лучше и проще для аудитории в возрасте 16-23 лет. Мы - будущее, хотя нет, мы - настоящее!'} 
+              owner={'livermoney'} 
+              tag={'Аналитика Бизнес'}
+            />
         </div>
     </>
   )
