580fe45f5c383ca6ab985073ab35e7bdf95d0f0a
Pavel <noonno20445@yandex.ru>
2023-09-10 22:57:23 +0300

    prev layout

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -8,11 +8,8 @@ import "../pages/App.scss"
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
       <Lk />
-=======
       <Main />
->>>>>>> 07e64ee2da0414d94c46cc2925e8253fee62ed57
     </div>
   );
 }
